aa55d22c7322dac907b883fa230af5495ddb7ff4
dustine32 <dustine32@gmail.com>
2019-02-14 22:25:04 -0800

    Triple merging for issue #11. Needs cleaning up.

--- gocamgen/gocamgen.py
+++ gocamgen/gocamgen.py
@@ -263,31 +263,56 @@ class AssocGoCamModel(GoCamModel):
         for a in self.associations:
 
             annoton = Annoton(a["subject"]["id"], [a])
-
-            self.declare_class(annoton.enabled_by)
-            gp_uri = self.declare_individual(annoton.enabled_by)
             term = a["object"]["id"]
-            self.declare_class(term)
-            term_uri = self.declare_individual(term)
 
             # Paul's current rules are based on aspect, similar to rdfgen's current state, which may change
             # since relation is explicitly stated in GPAD
             # Standardize aspect using GPAD relations?
 
+            matched_triples = []
             aspect_triples = []
             for q in a["qualifiers"]:
                 if q == "enables":
-                    aspect_triples.append(self.writer.emit(term_uri, ENABLED_BY, gp_uri))
+                    found_triples = self.triples_by_ids(term, ENABLED_BY, annoton.enabled_by)
+                    print("# of reused triples: {}".format(len(found_triples)))
+                    if len(found_triples) > 0:
+                        matched_triples.append(found_triples[0])
+                    else:
+                        gp_uri = self.declare_individual(annoton.enabled_by)
+                        term_uri = self.declare_individual(term)
+                        aspect_triples.append(self.writer.emit(term_uri, ENABLED_BY, gp_uri))
                 elif q == "involved_in":
-                    self.declare_class(upt.molecular_function)
-                    mf_root_uri = self.declare_individual(upt.molecular_function)
-                    aspect_triples.append(self.writer.emit(mf_root_uri, ENABLED_BY, gp_uri))
-                    aspect_triples.append(self.writer.emit(mf_root_uri, PART_OF, term_uri))
+                    make_new = True
+                    found_triples = self.triples_by_ids(upt.molecular_function, ENABLED_BY, annoton.enabled_by)
+                    if len(found_triples) > 0:
+                        for mf_triple in found_triples:
+                            found_mf_uri = mf_triple[0]
+                            found_triples = self.triples_by_ids(found_mf_uri, PART_OF, term)
+                            if len(found_triples) > 0:
+                                # Found both triples. Add em and get out of here
+                                bp_triple = found_triples[0]
+                                matched_triples.append(mf_triple)
+                                matched_triples.append(bp_triple)
+                                make_new = False
+                                break
+                    if make_new:
+                        mf_root_uri = self.declare_individual(upt.molecular_function)
+                        gp_uri = self.declare_individual(annoton.enabled_by)
+                        term_uri = self.declare_individual(term)
+                        aspect_triples.append(self.writer.emit(mf_root_uri, ENABLED_BY, gp_uri))
+                        aspect_triples.append(self.writer.emit(mf_root_uri, PART_OF, term_uri))
                 elif q == "NOT":
                     # Try it in UI and look at OWL
                     do_stuff = 1
                 else:
-                    aspect_triples.append(self.writer.emit(gp_uri, URIRef(expand_uri_wrapper(self.relations_dict[q])), term_uri))
+                    relation_uri = URIRef(expand_uri_wrapper(self.relations_dict[q]))
+                    found_triples = self.triples_by_ids(annoton.enabled_by, relation_uri, term)
+                    if len(found_triples) > 0:
+                        matched_triples.append(found_triples[0])
+                    else:
+                        gp_uri = self.declare_individual(annoton.enabled_by)
+                        term_uri = self.declare_individual(term)
+                        aspect_triples.append(self.writer.emit(gp_uri, relation_uri, term_uri))
 
             annot_date = "{0:%Y-%m-%d}".format(datetime.datetime.strptime(a["date"], "%Y%m%d"))
             source_line = a["source_line"].rstrip().replace("\t", " ")
@@ -303,6 +328,13 @@ class AssocGoCamModel(GoCamModel):
             for atr in aspect_triples:
                 axiom_id = self.add_axiom(atr)
                 self.add_evidence(axiom_id, a["evidence"]["type"],
+                               a["evidence"]["has_supporting_reference"],
+                                  contributors=contributors,
+                                  date=annot_date,
+                                  comment=source_line)
+            for mt in matched_triples:
+                mt_axiom_id = self.find_bnode(mt)
+                self.add_evidence(mt_axiom_id, a["evidence"]["type"],
                                a["evidence"]["has_supporting_reference"],
                                   contributors=contributors,
                                   date=annot_date,
