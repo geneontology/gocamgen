d3f66a1db76d662675da2250e2c8e3cdf258b7c9
dustine32 <dustine32@gmail.com>
2020-05-04 13:40:51 -0700

    setup.py fix README.md for twine

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     author_email="debert@usc.edu",
     description="Python library for constructing GO-CAM model RDF",
     long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
     url="https://github.com/dustine32/gocamgen",
     install_requires=[
         "ontobio==1.13.1",
